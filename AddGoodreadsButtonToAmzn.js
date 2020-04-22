7f802c6b00beff6731823f2f07955ec7ab5c841c
tantrumkiller <joshikshitij_13@yahoo.in>
2020-04-22 14:31:48 -0700

    Handle multiple page versions
    Handle:
    1. EBook page
    2. New tabbed version of page

--- AddGoodreadsButtonToAmzn.js
+++ AddGoodreadsButtonToAmzn.js
@@ -2,25 +2,48 @@
 // @name     Add Goodreads Button
 // @version  1
 // @grant    none
-// @include  https://www.amazon.com/*
+// @include  https://www.amazon.*/*
 // ==/UserScript==
 
-var bookTitleSection = document.getElementById("booksTitle");
-var title = document.getElementById("productTitle");
+var titleSection = null;
+var title = null;
 
-if(bookTitleSection && title ) {
+var bookTitleSection = document.getElementById("booksTitle");
+
+//Older stanard page
+if(bookTitleSection) {
+	title = document.getElementById("productTitle");
+  if(!title) {
+		title = document.getElementById("ebooksProductTitle");
+  	titleSection = bookTitleSection
+	}
+  
+  titleSection = bookTitleSection
+}
+
+//New tabed version
+if(!title) {
+	var productTitle = document.getElementById("productTitle");
+	var bookEdition = document.getElementById("bookEdition");
+  
+  if(bookEdition && productTitle) {
+  	title = productTitle.innerText
+    titleSection = document.getElementById("title_feature_div");
+  }
+}
+
+if(title) {
   var input=document.createElement("input");
   input.type="button";
   input.value="Find on Goodreads";
   input.onclick = goToGoodreads;
-  document.getElementById("booksTitle").appendChild(input);
-
-
+  titleSection.appendChild(input);
 
   var url_string = location.href
   var url = new URL(url_string);
 
   function goToGoodreads(){
-    window.location.replace("https://www.goodreads.com/search?q=" + title.innerText);
+    var win = window.open("https://www.goodreads.com/search?q=" + title.innerText, '_blank');
+    wun.focus();
   }
 }
